ff895054a66f627957d3da7785386aaa89bdd1ca
Hannah Muetzel <muetzel.2@wright.edu>
2016-04-06 19:28:28 -0400

    Begin Project
    Imma commit dis stuff.

---
+++ AVLTree/AVLTree.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class AVLTree {
+public:
+
+	AVLTree();
+	~AVLTree();
+	//TODO: bool AVLTree::insert(int key, int value);
+	//TODO: int AVLTree::getHeight();
+	//TODO: int AVLTree::getSize();
+	//TODO: void AVLTree::print();
+	//TODO: bool AVLTree::find(int key, int& value);
+	//TODO: vector<int> AVLTree::findRange(int lowkey, int highkey);
+
+	//TODO: recursive insert
+	//TODO: double rotate
+	//TODO: double rotate
+	//TODO: not be a little bitch?
+
+private:
+
+};
---
+++ AVLTree/Node.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Node {
+public:
+
+	Node();
+	~Node();
+
+
+
+private:
+
+};
